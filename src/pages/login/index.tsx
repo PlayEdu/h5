566e08556b9727217b7d0d7afc84c9a1d7b58d0e
禺狨 <18119604035@163.com>
2023-06-30 16:44:54 +0800

    我的、登录页面优化

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -190,12 +190,12 @@ const LoginPage = () => {
             登 录
           </Button>
         </div>
-        <div className={styles["support-box"]}>
-          <i
-            style={{ fontSize: 20, color: "rgba(0, 0, 0, 0.3)" }}
-            className="iconfont icon-playedu"
-          ></i>
-        </div>
+      </div>
+      <div className={styles["support-box"]}>
+        <i
+          style={{ fontSize: 20, color: "rgba(0, 0, 0, 0.3)" }}
+          className="iconfont icon-playedu"
+        ></i>
       </div>
     </div>
   );
