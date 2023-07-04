ae4cdb0de1aa043f93a7275fb6ac948ed6470e22
none <none>
2023-07-04 16:41:13 +0800

    fixed: loading页面的高度

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -2,7 +2,7 @@ import { SpinLoading } from "antd-mobile";
 
 const LoadingPage = () => {
   return (
-    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+    <div style={{ width: "100vw", height: "100vh",  display: "flex", justifyContent: "center" }}>
       <SpinLoading color="primary" />
     </div>
   );
